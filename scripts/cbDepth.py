75cdb7c33c266f216265539a8c8129356b83d779
wowyunDBL <wowyunpg@gmail.com>
2021-03-28 15:47:34 +0800

    0328

---
+++ scripts/cbDepth.py
@@ -0,0 +1,34 @@
+#!usr/bin/env python
+import rospy
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+import csv
+
+def msg2CV(msg):
+    bridge = CvBridge()
+    try:
+        image = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+        return image
+    except CvBridgeError as e:
+        print(e)
+        return
+
+def cbDepth(msg):
+    print("callback!")
+    image = msg2CV(msg)
+    cv2.imshow('raw image', image)
+    cv2.circle(image,(0,0),15,(255,0,0),-1)
+    cv2.waitKey(1)
+    
+    with open('/home/anny/RealSense/catkin_ws/src/depth_app/scripts/output_two.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(image)
+    
+
+
+if __name__ == "__main__":
+    rospy.init_node("depthHandler", anonymous=True)
+    subDepth = rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, cbDepth)
+    print("successfully initialized!")
+    rospy.spin()
